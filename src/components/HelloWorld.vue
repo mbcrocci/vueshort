20462fc941bedc9a283cbc71d103e18dd728b74c
Maurizio Crocci <mbcrocci@gmail.com>
2020-02-09 19:38:27 +0000

    Finish first stage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,107 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+  <v-card class="elevation-12">
+    <v-toolbar color="primary" dark flat>
+      <v-spacer />
+      <v-toolbar-title>VueShort</v-toolbar-title>
+      <v-spacer />
+    </v-toolbar>
+      <v-card-text>
+        <v-text-field
+          label="Url"
+          placeholder="http://google.com"
+          outlined
+          v-model="url"
+          v-on:keyup.enter="saveOrGoto"
+        />
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="primary" v-on:click="save">Save</v-btn>
+      <v-btn color="primary" v-on:click="goto">Go</v-btn>
+    </v-card-actions>
+    <v-divider />
+    <v-list>
+      <template v-for="url in this.urls$">
+        <v-list-item :key="url.shortid">
+          <v-list-item-title v-html="url.shortid"></v-list-item-title>
+          <v-list-item-content v-html="url.longUrl"></v-list-item-content>
+          {{url.longUrl}}
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-card>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import Vue from 'vue';
+import { timer } from 'rxjs';
+import { switchMap, concatMap, catchError } from 'rxjs/operators';
+import { fromFetch } from 'rxjs/fetch';
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+function isUrl(url: string) {
+  const urlRegex = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+
+  return urlRegex.test(url);
 }
+
+function prepareUrl(url: string) {
+  let u = url;
+  if (!url.includes('http://')) {
+    u = `http:///${url}`;
+  }
+
+  return u;
+}
+
+const apiUrl = 'http://localhost:3000/urls';
+
+export default Vue.extend({
+  data() {
+    return {
+      url: '',
+    };
+  },
+  name: 'HelloWorld',
+  subscriptions() {
+    const urls$ = timer(0, 1000)
+      .pipe(concatMap(() => fromFetch(`${apiUrl}?filter[limit]=10&filter[order]=date DESC`)))
+      .pipe(switchMap((response: Response) => response.json()));
+
+    return {
+      urls$,
+    };
+  },
+  methods: {
+    save() {
+      fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          longUrl: this.url,
+        }),
+      })
+        .catch((reason: any) => console.log(reason));
+    },
+    goto() {
+      fetch(`${apiUrl}/${this.url}`)
+        .then((resp: Response) => resp.json())
+        .then((data: any) => {
+          const url = prepareUrl(data.longUrl);
+          window.location.href = url;
+        })
+        .catch((reason: any) => console.log(reason));
+    },
+    saveOrGoto() {
+      if (isUrl(this.url)) {
+        this.save();
+      } else {
+        this.goto();
+      }
+    },
+  },
+});
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
+import Rx from 'rxjs';
+import VueRx from 'vue-rx';
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+import vuetify from './plugins/vuetify';
+
+Vue.use(VueRx, Rx);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
